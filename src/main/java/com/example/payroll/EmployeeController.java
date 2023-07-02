a33873a4c90e8ed83786022f21a65b9327336a24
María Fernanda Rios Cárdenas <maria.rios@sofka.com.co>
2023-07-01 22:36:19 -0500

    con SprintHATEOAS el controlador produce contenido hipermedia

--- src/main/java/com/example/payroll/EmployeeController.java
+++ src/main/java/com/example/payroll/EmployeeController.java
@@ -24,8 +24,11 @@ class EmployeeController {
 
     private final EmployeeRepository repository;
 
-    EmployeeController(EmployeeRepository repository){
+    private final EmployeeModelAssembler assembler;
+
+    EmployeeController(EmployeeRepository repository, EmployeeModelAssembler assembler){
         this.repository = repository;
+        this.assembler =assembler;
     }
 
     @GetMapping("/employees/{id}")
@@ -34,18 +37,14 @@ class EmployeeController {
         Employee employee = repository.findById(id) //
                 .orElseThrow(() -> new EmployeeNotFoundException(id));
 
-        return EntityModel.of(employee, //
-                linkTo(methodOn(EmployeeController.class).oneEmployee(id)).withSelfRel(),
-                linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
+        return assembler.toModel(employee);
     }
 
 
     @GetMapping("/employees")
     CollectionModel<EntityModel<Employee>> all(){
         List<EntityModel<Employee>> employess = repository.findAll().stream()
-                .map(employee -> EntityModel.of(employee,
-                        linkTo(methodOn(EmployeeController.class).oneEmployee(employee.getId())).withSelfRel(),
-                        linkTo(methodOn(EmployeeController.class).all()).withRel("employess")))
+                .map(assembler::toModel)
                 .collect(Collectors.toList());
         return CollectionModel.of(employess,
                 linkTo(methodOn(EmployeeController.class).all()).withSelfRel()
